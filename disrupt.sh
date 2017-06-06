8e3fc471d8c99f607b565a96d02e122911b92cb9
Lucas Fryzek <nercos7@gmail.com>
2017-06-05 23:59:45 -0400

    Add html content type to email
    Adds support to sending emails to gmail addresses

--- disrupt.sh
+++ disrupt.sh
@@ -1,5 +1,5 @@
-tmpstring=$(cat template)
 randomfacts=$(shuf -n 1 lucashifacts)
-tmpstring="$tmpstring $randomfacts"
+facthtml="$(echo ${randomfacts} | sed 's/&/\&amp;/g; s/</\&lt;/g; s/>/\&gt;/g; s/"/\&quot;/g; s/'"'"'/\&#39;/g')"
+tmpstring=$(eval "echo \"$(cat template)\"")
 
-echo "$tmpstring" | /usr/bin/mail -s "lucashi fact of the day" $1
+echo "$tmpstring" | /usr/bin/mutt -e 'set content_type=text/html' -s "lucashi fact of the day" $1

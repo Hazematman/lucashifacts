2d296c5d67564c5107e88bba29b9f967cfd89c28
caelink <caelink@gmail.com>
2017-06-05 23:10:43 -0400

    made it clone clean errytime

---
+++ main.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git clone https://github.com/Caelink/lucashifacts.git tmp_lucashi_pls_no_collide
+
+cd tmp_lucashi_pls_no_collide
+./spam_me.sh
+cd -
+rm -rf tmp_lucashi_pls_no_collide
+

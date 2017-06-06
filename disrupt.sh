267176217dd152b8de3142d2854a6eb9f4d206f6
Lucas Fryzek <nercos7@gmail.com>
2017-06-06 00:41:46 -0400

    Fix CI issues

--- disrupt.sh
+++ disrupt.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 randomfacts="$(shuf -n 1 lucashifacts)"
-facthtml="$(echo ${randomfacts} | sed 's/&/\&amp;/g; s/</\&lt;/g; s/>/\&gt;/g; s/"/\&quot;/g; s/'"'"'/\&#39;/g')"
-export tmpstring="$(eval "echo \"$(cat template)\"")"
+export facthtml="$(echo ${randomfacts} | sed 's/&/\&amp;/g; s/</\&lt;/g; s/>/\&gt;/g; s/"/\&quot;/g; s/'"'"'/\&#39;/g')"
+export tmpstring
+tmpstring="$(eval "echo \"$(cat template)\"")"
 
 if [ "$#" -eq 1 ]; then
     echo "$tmpstring" | /usr/bin/mutt -e 'set content_type=text/html' -s "lucashi fact of the day" "$1"
--- spam_me.sh
+++ spam_me.sh
@@ -2,8 +2,8 @@
 
 source ./config.sh
 
-if [ -r $EMAILS ]; then
-    cat "$EMAILS" | xargs -n 1 "$MAIL_SCRIPT"
+if [ -r "$EMAILS" ]; then
+    xargs -n 1 "$MAIL_SCRIPT" < "$EMAILS"
 else
     ./$MAIL_SCRIPT
 fi

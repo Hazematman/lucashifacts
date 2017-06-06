4f125e828b8c281c76f2488585fe1004c3e592e1
Lucas Fryzek <nercos7@gmail.com>
2017-06-06 00:41:46 -0400

    Fix CI issues

--- disrupt.sh
+++ disrupt.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# Export all these variables for SC2155 rule
+export facthtml
+export tmpstring
+
 randomfacts="$(shuf -n 1 lucashifacts)"
-facthtml="$(echo ${randomfacts} | sed 's/&/\&amp;/g; s/</\&lt;/g; s/>/\&gt;/g; s/"/\&quot;/g; s/'"'"'/\&#39;/g')"
-export tmpstring="$(eval "echo \"$(cat template)\"")"
+facthtml="$(echo "${randomfacts}" | sed 's/&/\&amp;/g; s/</\&lt;/g; s/>/\&gt;/g; s/"/\&quot;/g; s/'"'"'/\&#39;/g')"
+tmpstring="$(eval "echo \"$(cat template)\"")"
 
 if [ "$#" -eq 1 ]; then
     echo "$tmpstring" | /usr/bin/mutt -e 'set content_type=text/html' -s "lucashi fact of the day" "$1"

467cfb3942f650fd92b838e3e2f277402874d433
Lucas Fryzek <nercos7@gmail.com>
2017-06-06 00:29:40 -0400

    Add 'secret file' support for emails to send to

--- disrupt.sh
+++ disrupt.sh
@@ -2,6 +2,10 @@
 
 randomfacts="$(shuf -n 1 lucashifacts)"
 facthtml="$(echo ${randomfacts} | sed 's/&/\&amp;/g; s/</\&lt;/g; s/>/\&gt;/g; s/"/\&quot;/g; s/'"'"'/\&#39;/g')"
-tmpstring="$(eval "echo \"$(cat template)\"")"
+export tmpstring="$(eval "echo \"$(cat template)\"")"
 
-echo "$tmpstring" | /usr/bin/mutt -e 'set content_type=text/html' -s "lucashi fact of the day" $1
+if [ "$#" -eq 1 ]; then
+    echo "$tmpstring" | /usr/bin/mutt -e 'set content_type=text/html' -s "lucashi fact of the day" "$1"
+else
+    echo "$tmpstring"
+fi
--- spam_me.sh
+++ spam_me.sh
@@ -2,4 +2,8 @@
 
 source ./config.sh
 
-< $EMAILS xargs -n 1 $MAIL_SCRIPT
+if [ -r $EMAILS ]; then
+    cat "$EMAILS" | xargs -n 1 "$MAIL_SCRIPT"
+else
+    ./$MAIL_SCRIPT
+fi
